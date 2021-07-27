a849d075d787ce669d49cde4fcc5e57d814de167
Roomsie Jones <seangravito@gmail.com>
2021-07-27 18:48:33 -0500

    added the requirements from the comments in index.js

--- models/index.js
+++ models/index.js
@@ -12,6 +12,19 @@ const ProductTag = require('./ProductTag');
 
 // Tags belongToMany Products (through ProductTag)
 
+Product.belongsTo(Category);
+Category.hasMany(Product);
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+  foreignKey: 'product_id',
+});
+
+
+Tag.belongsToMany(Product, {
+  through: ProductTag,
+  foreignKey: 'tag_id',
+});
+
 module.exports = {
   Product,
   Category,
