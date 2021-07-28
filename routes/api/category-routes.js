a7097bd24961a179c52696ac17c302fb6397601e
Roomsie Jones <seangravito@gmail.com>
2021-07-27 21:24:48 -0500

    added the :id route to display when the id is called in the address.

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -6,11 +6,34 @@ const { Category, Product } = require('../../models');
 router.get('/', (req, res) => {
   // find all categories
   // be sure to include its associated Products
+  Category.findAll({
+    include: {
+      model: Product,
+      attributes: ['id','product_name','price','stock','category_id']
+    }
+  }).then(function (dbCategoryData) {
+    res.json(dbCategoryData);
+  })
+  .catch(function (err) {
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
 
 router.get('/:id', (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  Category.findOne({
+    where: {
+      id: req.params.id
+    },
+    include: [
+      {
+        model: Product,
+        attributes: ['id','product_name','price','stock','category_id']
+      }
+    ]
+  })
 });
 
 router.post('/', (req, res) => {
